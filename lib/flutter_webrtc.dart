7bf979ecacab543f537016af2d24a0f8a97e1c68
Hoang Cuong <cuong.hoang@manabie.com>
2021-05-24 09:42:14 +0700

    remove all native

--- lib/flutter_webrtc.dart
+++ lib/flutter_webrtc.dart
@@ -19,9 +19,7 @@ export 'src/interface/rtc_stats_report.dart';
 export 'src/interface/rtc_track_event.dart';
 export 'src/media_devices.dart';
 export 'src/media_recorder.dart';
-export 'src/native/rtc_peerconnection_factory.dart'
-    if (dart.library.html) 'src/web/rtc_peerconnection_factory.dart';
-export 'src/native/rtc_video_view_impl.dart'
-    if (dart.library.html) 'src/web/rtc_video_view_impl.dart';
-export 'src/native/utils.dart' if (dart.library.html) 'src/web/utils.dart';
+export 'src/web/rtc_peerconnection_factory.dart';
+export 'src/web/rtc_video_view_impl.dart';
+export 'src/web/utils.dart';
 export 'src/rtc_video_renderer.dart';
